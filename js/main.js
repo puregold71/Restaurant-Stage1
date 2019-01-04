2bb48c1d1af07735d9b9d010fdf4803b91f1d97b
Julie Singh <julie@juliesingh.tech>
2019-01-03 22:02:06 -0500

    fix bug load copy files

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added
+  initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -161,7 +161,6 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name} in ${restaurant.neighborhood}`;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -198,7 +197,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-}
+} 
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -209,3 +208,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
